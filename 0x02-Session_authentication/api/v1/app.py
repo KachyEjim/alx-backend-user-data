fb72164e1d58956a5e805886ec0e878b9795e012
kachyEjim <ejimovcgmail.com>
2024-09-14 11:01:46 +0100

    All tasks done

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,6 +32,11 @@ if AUTH_TYPE == "session_exp_auth":
 
     auth = SessionExpAuth()
 
+if AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+
+    auth = SessionDBAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -7,6 +7,15 @@ class SessionDBAuth(SessionExpAuth):
     """SessionDBaUTH CLASS"""
 
     def create_session(self, user_id=None):
+        """Creates  session"""
         session_id = super().create_session(user_id)
         return session_id
+
     def user_id_for_session_id(self, session_id=None):
+        """Gets a user by the session_id"""
+        user_id = super().user_id_for_session_id(session_id)
+        return user_id
+
+    def destroy_session(self, request=None):
+        """Destroies a users session"""
+        super().destroy_session(request)
